a1c7f0486a3ad8419d4fb54be7cfba1415a4d8a9
ErichMeisner <em031@hdm-stuttgart.de>
2015-11-18 12:18:06 +0100

    Klasse TextyAdministrationImpl angelegt. Die Interfaces TextyAdministration.java & TextyAdministrationAsync.java angelegt

---
+++ Texty/src/hdm/itprojekt/texty/server/TextyAdministrationImpl.java
@@ -0,0 +1,10 @@
+package hdm.itprojekt.texty.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import hdm.itprojekt.texty.shared.TextyAdministration;
+
+public class TextyAdministrationImpl extends RemoteServiceServlet 
+	implements TextyAdministration  {
+
+}
