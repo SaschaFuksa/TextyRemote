4a9c6c21ed4318ce880d931774caa274619b2778
SaschaFuksa <sf073@hdm-stuttgart.de>
2015-11-19 13:08:16 +0100

    Added edit-button as sample. Added Mousehover in css, but this will be edited. As example, it works!

--- Texty/src/hdm/itprojekt/texty/client/Texty.java
+++ Texty/src/hdm/itprojekt/texty/client/Texty.java
@@ -1,12 +1,18 @@
 package hdm.itprojekt.texty.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
 //import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 //import hdm.itprojekt.texty.shared.*;
 
 public class Texty implements EntryPoint {
 	
+	private VerticalPanel buttonPanel = new VerticalPanel();
+	private Button testButton = new Button();
 	
 	/*private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
@@ -18,5 +24,8 @@ public class Texty implements EntryPoint {
 
 	public void onModuleLoad() {
 		
+		testButton.getElement().setId("editButton");
+		buttonPanel.add(testButton);
+		RootPanel.get("testButtonContainer").add(buttonPanel);
 	}
 }
