0a972ca5c60eec41d5288f5c68841103036610fa
FriedrichSchneider <fs.20@web.de>
2015-11-19 23:19:30 +0100

    Anlegen der verschiedenen Report Klassen für die Klassifizierung von Report Objeten (im .shared.report Package)

---
+++ Texty/src/hdm/itprojekt/texty/shared/report/AllMessagesByDateReport.java
@@ -0,0 +1,32 @@
+package hdm.itprojekt.texty.shared.report;
+
+import java.io.Serializable;
+
+/**
+ * Report, der alle Nachrichten des abfragenden Benutzers ab einem bestimmten Datum darstellt.
+ * Diese Klasse existiert nur um den Typ eines erstellten Reports
+ * deklarieren zu können, implementiert daher keine Attribute oder Methoden.
+ * Diese werden in den Superklassen bereits bereitgestellt.
+ *
+ * @author Friedrich Schneider
+ *
+ */
+
+
+/*
+ * TODO SUperklassen bauen, von denen die einzelnen Reportarten erben!
+ * 
+ * @see Bankprojekt/SimpleReport,CompositeReport,usw.
+ */
+
+public class AllMessagesByDateReport
+	implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
---
+++ Texty/src/hdm/itprojekt/texty/shared/report/AllMessagesFromUserByDateReport.java
@@ -0,0 +1,29 @@
+package hdm.itprojekt.texty.shared.report;
+
+import java.io.Serializable;
+
+/**
+ * Report, der alle Nachrichten eines ausgewählten Benutzers ab einem bestimmten Datum darstellt.
+ * Diese Klasse existiert nur um den Typ eines erstellten Reports
+ * deklarieren zu können, implementiert daher keine Attribute oder Methoden.
+ * Diese werden in den Superklassen bereits bereitgestellt.
+ *
+ * @author Friedrich Schneider
+ *
+ */
+
+/*
+ * TODO SUperklassen bauen, von denen die einzelnen Reportarten erben!
+ * 
+ * @see Bankprojekt/SimpleReport,CompositeReport,usw.
+ */
+
+public class AllMessagesFromUserByDateReport
+implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
