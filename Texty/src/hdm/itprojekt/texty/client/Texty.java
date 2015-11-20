9258f933cfb9e1740698e9ddce6016c35e78b239
ErichMeisner <em031@hdm-stuttgart.de>
2015-11-20 14:17:06 +0100

    Menübar mit Untermenüs eingefügt.

--- Texty/src/hdm/itprojekt/texty/client/Texty.java
+++ Texty/src/hdm/itprojekt/texty/client/Texty.java
@@ -1,10 +1,13 @@
 package hdm.itprojekt.texty.client;
 
+import hdm.itprojekt.texty.client.gui.TextyReport;
+
 import java.util.ArrayList;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -34,7 +37,64 @@ public class Texty implements EntryPoint {
 
 	public void onModuleLoad() {
 		
-		//Anlegen der Variable rootPanel
+		Command cmd = new Command() {
+		      public void execute() {
+		        Window.alert("You selected a menu item!");
+		      }
+		    };
+		    
+		 Command cmdReport = new Command() {
+			      public void execute() {
+			    	  //http://127.0.0.1:8888/TextyReport.java;
+			    	  TextyReport report= new TextyReport();
+			    	  report.onModuleLoad();
+			      }
+			    };
+		    
+		    // Make some sub-menus that we will cascade from the top menu.
+		    MenuBar subMenu1 = new MenuBar(true);
+		    subMenu1.addItem("Public Conversation", cmd);
+		    subMenu1.addItem("Private Conversations", cmd);
+		    
+		    // Make some sub-menus that we will cascade from the top menu.
+		    MenuBar fooMenu = new MenuBar(true);
+		    fooMenu.addItem("New Conversation", cmd);
+		    fooMenu.addItem("Show Conversations", subMenu1);
+		   
+
+		 // Make some sub-menus that we will cascade from the top menu.
+		    MenuBar subMenu2 = new MenuBar(true);
+		    subMenu2.addItem("Create", cmd);
+		    subMenu2.addItem("Show", cmd);
+		    
+		 // Make some sub-menus that we will cascade from the top menu.
+		    MenuBar subMenu3 = new MenuBar(true);
+		    subMenu3.addItem("Create", cmd);
+		    subMenu3.addItem("Show", cmd);
+		    
+		    MenuBar barMenu = new MenuBar(true);
+		    barMenu.addItem("Hashtagsubscription", subMenu2);
+		    barMenu.addItem("Usersubscription", subMenu3);
+		    
+
+		    // Make a new menu bar, adding a few cascading menus to it.
+		    MenuBar menu = new MenuBar();
+		    menu.addItem("Conversation", fooMenu);
+		    menu.addItem("Subscription", barMenu);
+		    menu.addItem("Report",cmdReport);
+		    
+		    
+
+		    // Add it to the root panel.
+		    RootPanel.get("Menue").add(menu);
+	}
+		
+
+		
+		
+		
+		
+		/*//Anlegen der Variable rootPanel
 		RootPanel rootPanel = RootPanel.get();
 		
 		testButton.getElement().setId("editButton");
@@ -82,10 +142,10 @@ public class Texty implements EntryPoint {
 		
 		//Kann vielleicht noch gebraucht werden
 		//MultiWordSuggestOracle suggestBox = new MultiWordSuggestOracle();
-		
+*/		
 		
 	
 		
 		
 	}
-}
+
