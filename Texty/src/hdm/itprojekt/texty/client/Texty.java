aa154d669497c3c888cc36f6d44a226a6c1f45fc
DanielSeliger <ds138@hdm-stuttgart.de>
2015-12-05 23:25:23 +0100

    Menubar ausgelagert, Variablen angepasst

--- Texty/src/hdm/itprojekt/texty/client/Texty.java
+++ Texty/src/hdm/itprojekt/texty/client/Texty.java
@@ -9,6 +9,7 @@ import hdm.itprojekt.texty.client.gui.ShowPrivateConversation;
 import hdm.itprojekt.texty.client.gui.ShowPublicConversation;
 import hdm.itprojekt.texty.client.gui.ShowUserSubscription;
 import hdm.itprojekt.texty.client.gui.TextyCommand;
+import hdm.itprojekt.texty.client.gui.TextyMenu;
 import hdm.itprojekt.texty.shared.LoginService;
 import hdm.itprojekt.texty.shared.LoginServiceAsync;
 
@@ -34,10 +35,11 @@ public class Texty implements EntryPoint {
 	private HorizontalPanel loginPanel = new HorizontalPanel();
 	private Label nickname = new Label();
 	private Anchor signOutLink = new Anchor("Sign Out");
+	private TextyMenu menu = new TextyMenu();
 	
-	private TextyCommand command = new TextyCommand();
-
 	public void onModuleLoad() {
+		
+		menu.execute();
 
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
 		loginService.login(GWT.getHostPageBaseURL(),
@@ -69,45 +71,7 @@ public class Texty implements EntryPoint {
 		});
 
 		
-		// Submenu fuer Conversation
-		MenuBar subMenu1 = new MenuBar(true);
-		subMenu1.addItem("Public Conversation", command.getCommand(new ShowPublicConversation()));
-		subMenu1.addItem("Private Conversations", command.getCommand(new ShowPrivateConversation()));
-		subMenu1.setStyleName("menubar");
 		
-		NewConversation newConv = new NewConversation();
-		// Menu für Conversation
-		MenuBar fooMenu = new MenuBar(true);
-		fooMenu.addItem("New Conversation", command.getCommand(new NewConversation()));
-		fooMenu.addItem("Show Conversations", subMenu1);
-		fooMenu.setStyleName("menubar");
-
-		// Submenu fuer Hashtagsubscription
-		MenuBar subMenu2 = new MenuBar(true);
-		subMenu2.addItem("Create",  command.getCommand(new CreateHashtagSubscription()));
-		subMenu2.addItem("Show", command.getCommand(new ShowHashtagSubscription()));
-		subMenu2.setStyleName("menubar");
-
-		// Submenu fuer Usersubscription
-		MenuBar subMenu3 = new MenuBar(true);
-		subMenu3.addItem("Create", command.getCommand(new CreateUserSubscription()));
-		subMenu3.addItem("Show", command.getCommand(new ShowUserSubscription()));
-		subMenu3.setStyleName("menubar");
-
-		// Menu für Subscription
-		MenuBar barMenu = new MenuBar(true);
-		barMenu.addItem("Hashtagsubscription", subMenu2);
-		barMenu.addItem("Usersubscription", subMenu3);
-		barMenu.setStyleName("menubar");
-
-		// Komplette Menübar
-		MenuBar menu = new MenuBar();
-		menu.addItem("Conversation", fooMenu);
-		menu.addItem("Subscription", barMenu);
-		menu.addItem("ReportGenerator", command.getReportCommand());
-		menu.setStyleName("menubar");
-
-		RootPanel.get("Menu").add(menu);
 	}
 
 	private void createLoginPanel() {
---
+++ Texty/src/hdm/itprojekt/texty/client/gui/TextyMenu.java
@@ -0,0 +1,58 @@
+package hdm.itprojekt.texty.client.gui;
+
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class TextyMenu {
+
+	private TextyCommand command = new TextyCommand();
+
+	public void execute() {
+
+		// Submenu fuer Conversation
+		MenuBar subMenuConversation = new MenuBar(true);
+		subMenuConversation.addItem("Public Conversation",
+				command.getCommand(new ShowPublicConversation()));
+		subMenuConversation.addItem("Private Conversations",
+				command.getCommand(new ShowPrivateConversation()));
+		subMenuConversation.setStyleName("menubar");
+
+		// Haupt-Menu fuer Conversation
+		MenuBar mainMenuConversation = new MenuBar(true);
+		mainMenuConversation.addItem("New Conversation",
+				command.getCommand(new NewConversation()));
+		mainMenuConversation.addItem("Show Conversations", subMenuConversation);
+		mainMenuConversation.setStyleName("menubar");
+
+		// Submenu fuer Hashtagsubscription
+		MenuBar subMenuHashtagSubscription = new MenuBar(true);
+		subMenuHashtagSubscription.addItem("Create",
+				command.getCommand(new CreateHashtagSubscription()));
+		subMenuHashtagSubscription.addItem("Show",
+				command.getCommand(new ShowHashtagSubscription()));
+		subMenuHashtagSubscription.setStyleName("menubar");
+
+		// Submenu fuer Usersubscription
+		MenuBar subMenuUserSubscription = new MenuBar(true);
+		subMenuUserSubscription.addItem("Create",
+				command.getCommand(new CreateUserSubscription()));
+		subMenuUserSubscription.addItem("Show", command.getCommand(new ShowUserSubscription()));
+		subMenuUserSubscription.setStyleName("menubar");
+
+		// Haupt-Menu für Subscription
+		MenuBar mainMenuSubscription = new MenuBar(true);
+		mainMenuSubscription.addItem("Hashtagsubscription", subMenuHashtagSubscription);
+		mainMenuSubscription.addItem("Usersubscription", subMenuUserSubscription);
+		mainMenuSubscription.setStyleName("menubar");
+
+		// Komplette Menübar
+		MenuBar menu = new MenuBar();
+		menu.addItem("Conversation", mainMenuConversation);
+		menu.addItem("Subscription", mainMenuSubscription);
+		menu.addItem("ReportGenerator", command.getReportCommand());
+		menu.setStyleName("menubar");
+
+		RootPanel.get("Menu").add(menu);
+	}
+
+}
