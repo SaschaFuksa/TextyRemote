aee02874a05b32b932b0ff23741efb87ac88639e
SaschaFuksa <sf073@hdm-stuttgart.de>
2015-12-17 21:59:32 +0100

    Some small changes

--- Texty/src/hdm/itprojekt/texty/client/ReloadForm.java
+++ Texty/src/hdm/itprojekt/texty/client/ReloadForm.java
@@ -2,8 +2,6 @@ package hdm.itprojekt.texty.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
