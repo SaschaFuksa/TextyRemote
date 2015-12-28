8d1ffbffecd9631a4fe064720f443c65122909fb
SaschaFuksa <sf073@hdm-stuttgart.de>
2015-12-28 16:25:38 +0100

    Now you can answer to private messages

--- Texty/src/hdm/itprojekt/texty/client/SingleConversationViewer.java
+++ Texty/src/hdm/itprojekt/texty/client/SingleConversationViewer.java
@@ -1,14 +1,20 @@
 package hdm.itprojekt.texty.client;
 
+import hdm.itprojekt.texty.shared.TextyAdministrationAsync;
 import hdm.itprojekt.texty.shared.bo.Conversation;
+import hdm.itprojekt.texty.shared.bo.Message;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 
 public class SingleConversationViewer extends TextyForm {
 
 	private MessageForm message = new MessageForm();
-
 	private Conversation conversation = null;
+	private final TextyAdministrationAsync administration = ClientsideSettings
+			.getTextyAdministration();
 
 	public SingleConversationViewer(String headline) {
 		super(headline);
@@ -23,6 +29,22 @@ public class SingleConversationViewer extends TextyForm {
 	protected void run() {
 
 		showAllMessages();
+		message.sendButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				administration.addMessageToConversation(conversation, message.getText(), message.getHashtag(), new AsyncCallback<Message>() {
+							@Override
+							public void onFailure(Throwable caught) {
+
+							}
+
+							@Override
+							public void onSuccess(Message result) {
+								message.clearSelectedHashtag();
+							}
+						});
+			}
+		});
 		this.add(message);
 
 	}
