6050e8a6ac38e7557b61ceaf65b8f9b22a841b60
SaschaFuksa <sf073@hdm-stuttgart.de>
2015-12-29 13:52:58 +0100

    Added buttons to authors messages. Handler will be added as soon as possbile

--- Texty/src/hdm/itprojekt/texty/client/SingleConversationViewer.java
+++ Texty/src/hdm/itprojekt/texty/client/SingleConversationViewer.java
@@ -3,16 +3,31 @@ package hdm.itprojekt.texty.client;
 import hdm.itprojekt.texty.shared.TextyAdministrationAsync;
 import hdm.itprojekt.texty.shared.bo.Conversation;
 import hdm.itprojekt.texty.shared.bo.Message;
+import hdm.itprojekt.texty.shared.bo.User;
+
+import java.util.logging.Logger;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SingleConversationViewer extends TextyForm {
 
+	private static final Logger LOG = Logger
+			.getLogger(SingleConversationViewer.class.getSimpleName());
+
+	private VerticalPanel content = new VerticalPanel();
+	private ScrollPanel scroll = new ScrollPanel(content);
+	private FlexTable chatFlexTable = new FlexTable();
 	private MessageForm message = new MessageForm();
 	private Conversation conversation = null;
+	private static User currentUser = null;
 	private final TextyAdministrationAsync administration = ClientsideSettings
 			.getTextyAdministration();
 
@@ -28,33 +43,112 @@ public class SingleConversationViewer extends TextyForm {
 	@Override
 	protected void run() {
 
-		showAllMessages();
+		administration.getCurrentUser(new AsyncCallback<User>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				LOG.severe("Error: " + caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(User result) {
+				LOG.info("Success :" + result.getClass().getSimpleName());
+				SingleConversationViewer.currentUser = result;
+				showAllMessages();
+			}
+		});
 		message.sendButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				administration.addMessageToConversation(conversation, message.getText(), message.getHashtag(), new AsyncCallback<Message>() {
+				administration.addMessageToConversation(conversation,
+						message.getText(), message.getHashtag(),
+						new AsyncCallback<Message>() {
 							@Override
 							public void onFailure(Throwable caught) {
-
+								LOG.severe("Error: " + caught.getMessage());
 							}
 
 							@Override
 							public void onSuccess(Message result) {
+								LOG.info("Success :"
+										+ result.getClass().getSimpleName());
 								message.clearSelectedHashtag();
 							}
 						});
 			}
 		});
+
+		scroll.setStylePrimaryName("conversationScroll");
+		content.add(chatFlexTable);
+		this.add(scroll);
 		this.add(message);
 
 	}
 
 	private void showAllMessages() {
+
 		for (int i = 0; i < conversation.getListOfMessage().size(); i++) {
-			Label label = new Label(conversation.getListOfMessage().get(i)
+			FlexTable messageTable = new FlexTable();
+			Label text = new Label(conversation.getListOfMessage().get(i)
 					.getText());
-			this.add(label);
+			Label author = new Label();
+			author.setStylePrimaryName("authorPanel");
+
+			if (conversation.getListOfMessage().get(i).getAuthor().getId() == currentUser
+					.getId()) {
+				HorizontalPanel buttonPanel = new HorizontalPanel();
+				author.setText("You");
+				text.setStylePrimaryName("senderPanel");
+				messageTable.setStylePrimaryName("senderPanel");
+				Button deleteButton = new Button("");
+				Button editButton = new Button("");
+				buttonPanel.setStylePrimaryName("buttonPanel");
+				deleteButton.getElement().setId("deleteButton");
+				editButton.getElement().setId("editButton");
+				buttonPanel.add(deleteButton);
+				buttonPanel.add(editButton);
+				messageTable.setWidget(0, 0, author);
+				messageTable.setWidget(0, 1, buttonPanel);
+				messageTable.getFlexCellFormatter().setColSpan(1, 0, 2);
+				messageTable.setWidget(1, 0, text);
+				chatFlexTable.setWidget(i, 1, messageTable);
+			} else {
+				author.setText(conversation.getListOfMessage().get(i)
+						.getAuthor().getFirstName());
+				text.setStylePrimaryName("receiverPanel");
+				messageTable.setStylePrimaryName("receiverPanel");
+				messageTable.setWidget(0, 0, author);
+				messageTable.setWidget(1, 0, text);
+				chatFlexTable.setWidget(i, 0, messageTable);
+			}
+			// HorizontalPanel header = new HorizontalPanel();
+			// VerticalPanel panel = new VerticalPanel();
+			// Label text = new
+			// Label(conversation.getListOfMessage().get(i).getText());
+			// Label author = new Label();
+			// author.setStylePrimaryName("authorPanel");
+			// header.add(author);
+			// panel.add(header);
+			// panel.add(text);
+			//
+			// if (conversation.getListOfMessage().get(i).getAuthor().getId() ==
+			// currentUser.getId()){
+			// author.setText("You");
+			// text.setStylePrimaryName("senderPanel");
+			// panel.setStylePrimaryName("senderPanel");
+			// chatFlexTable.setWidget(i, 2, panel);
+			// Button delete = new Button("x");
+			// Button edit = new Button("+");
+			// header.add(edit);
+			// header.add(delete);
+			// }
+			// else {
+			// author.setText(conversation.getListOfMessage().get(i).getAuthor().getFirstName());
+			// text.setStylePrimaryName("receiverPanel");
+			// panel.setStylePrimaryName("receiverPanel");
+			// chatFlexTable.setWidget(i, 1, panel);
+			// }
 		}
+		scroll.scrollToBottom();
 	}
 
 }
