9ff26ac8602f0c9e2c023d959d63987794b4dfc6
SaschaFuksa <sf073@hdm-stuttgart.de>
2016-01-01 15:49:36 +0100

    Updated layout of conversationform

--- Texty/src/hdm/itprojekt/texty/client/ConversationForm.java
+++ Texty/src/hdm/itprojekt/texty/client/ConversationForm.java
@@ -6,9 +6,12 @@ import hdm.itprojekt.texty.shared.bo.User;
 
 import java.util.Vector;
 
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.shared.DateTimeFormat;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -19,6 +22,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class ConversationForm extends TextyForm {
 
 	private static Vector<Conversation> conList = new Vector<Conversation>();
+	private VerticalPanel mainPanel = new VerticalPanel();
 	private Label intro = new Label(
 			"Here you can read and reply to private conversations");
 	private VerticalPanel content = new VerticalPanel();
@@ -48,11 +52,23 @@ public class ConversationForm extends TextyForm {
 					}
 				});
 
+		this.getElement().setId("conversationForm");
+		mainPanel.getElement().setId("conversationWrapper");
 		scroll.getElement().setId("conversationScroll");
+		content.getElement().setId("conversationContent");
 		
-		this.add(getHeadline());
-		this.add(intro);
-		this.add(scroll);
+		mainPanel.add(getHeadline());
+		mainPanel.add(intro);
+		mainPanel.add(scroll);
+
+		this.add(mainPanel);
+		
+		Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+			public void execute() {
+				scroll.setHeight(mainPanel.getOffsetHeight() + "px");
+			}
+		});
+		
 	}
 
 	private String setMessageText(String text) {
@@ -91,50 +107,52 @@ public class ConversationForm extends TextyForm {
 	}
 
 	public void showConversation() {
-		for (int i = 0; i < conList.size(); i++) {
-			final Conversation conversation = conList.get(i);
-			VerticalPanel conversationPanel = new VerticalPanel();
-			HorizontalPanel headerPanel = new HorizontalPanel();
-			HorizontalPanel receiverPanel = new HorizontalPanel();
+		for (Conversation conversation : conList) {
+			final Conversation conversationView = conversation;
+			FlexTable chatFlexTable = new FlexTable();			
 			FocusPanel wrapper = new FocusPanel();
-			String receiver = setRecipient(conList.get(i).getLastMessage()
+			
+			String receiver = setRecipient(conversationView.getLastMessage()
 					.getListOfReceivers());
-			String text = setMessageText(conList.get(i).getLastMessage()
+			String text = setMessageText(conversationView.getLastMessage()
 					.getText());
-			// String date = DateTimeFormat.getFormat("yyyy-MM-dd").format(
-			// conList.get(i).getLastMessage().getDateOfCreation());
+			String date = DateTimeFormat.getFormat("yyyy-MM-dd").format(
+					conversationView.getLastMessage().getDateOfCreation());
 			Label textLabel = new Label(text);
 			Label receiverLabel = new Label(receiver);
 			Label authorLabel = new Label("Last message from: "
-					+ conList.get(i).getLastMessage().getAuthor()
+					+ conversationView.getLastMessage().getAuthor()
 							.getFirstName());
-			// Label dateLabel = new Label(date);
+			Label dateLabel = new Label(date);
 
 			authorLabel.getElement().setId("conversationHead");
 			receiverLabel.getElement().setId("conversationHead");
-			// dateLabel.setStylePrimaryName("conversationDate");
+			dateLabel.getElement().setId("conversationDate");
 			textLabel.getElement().setId("conversationBody");
-			conversationPanel.getElement().setId("conversation");
-
-			receiverPanel.add(authorLabel);
-			receiverPanel.add(receiverLabel);
-			headerPanel.add(receiverPanel);
-			// headerPanel.add(dateLabel);
-
-			conversationPanel.add(headerPanel);
-			conversationPanel.add(textLabel);
-
-			wrapper.add(conversationPanel);
+			
+			
 			wrapper.addClickHandler(new ClickHandler() {
 
 				@Override
 				public void onClick(ClickEvent event) {
 					RootPanel.get("Details").clear();
 					RootPanel.get("Details").add(
-							new SingleConversationViewer("Private Conversation", conversation));
+							new SingleConversationViewer("Private Conversation", conversationView));
 				}
 			});
+			
+			chatFlexTable.getElement().setId("conversation");
+			
+			chatFlexTable.getFlexCellFormatter().setColSpan(1, 0, 2);
+			chatFlexTable.getFlexCellFormatter().setColSpan(2, 0, 2);
+			
+			chatFlexTable.setWidget(0, 0, authorLabel);
+			chatFlexTable.setWidget(0, 1, dateLabel);
+			chatFlexTable.setWidget(1, 0, receiverLabel);
+			chatFlexTable.setWidget(2, 0, textLabel);
 
+			wrapper.add(chatFlexTable);
+			
 			content.add(wrapper);
 
 		}
