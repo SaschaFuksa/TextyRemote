bafb6547e652dcd27455625981fec0557fc71eba
DanielSeliger <ds138@hdm-stuttgart.de>
2016-01-02 14:22:50 +0100

    Startseite in der Klasse HomeForm überarbeitet, neue Strukturen angelegt

--- Texty/src/hdm/itprojekt/texty/client/HomeForm.java
+++ Texty/src/hdm/itprojekt/texty/client/HomeForm.java
@@ -1,9 +1,25 @@
 package hdm.itprojekt.texty.client;
 
+import java.util.Vector;
+
+import hdm.itprojekt.texty.shared.TextyAdministrationAsync;
+import hdm.itprojekt.texty.shared.bo.Hashtag;
+import hdm.itprojekt.texty.shared.bo.User;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
 public class HomeForm extends TextyForm {
 
+	private static Vector<User> userConList = new Vector<User>();
+	private static Vector<Hashtag> hashtagConList = new Vector<Hashtag>();
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private Label intro = new Label(
+			"Here you can see and select your subscribed Hashtags and User");
 	private FavoriteForm favoriteForm = new FavoriteForm("Chatroom");
 	CellTreeForm treeForm = new CellTreeForm("Tree");
+	private TextyAdministrationAsync administration = ClientsideSettings
+			.getTextyAdministration();
 
 	public HomeForm(String headline) {
 		super(headline);
@@ -12,10 +28,57 @@ public class HomeForm extends TextyForm {
 	@Override
 	protected void run() {
 
+		// Abonnierte User und deren Postings
+		administration.getAllSubscribedUsers(new AsyncCallback<Vector<User>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+
+			}
+
+			@Override
+			public void onSuccess(Vector<User> result) {
+				HomeForm.userConList = result;
+				showPublicConversation();
+
+			}
+		});
+
+		// Abonnierte Hashtags und deren Postings
+		administration.getAllSubscribedHashtags(new AsyncCallback<Vector<Hashtag>>() {
+					@Override
+					public void onFailure(Throwable caught) {
+
+					}
+
+					@Override
+					public void onSuccess(Vector<Hashtag> result) {
+						HomeForm.hashtagConList = result;
+						showHashtagSubscriptions();
+
+					}
+
+				});
+
+		mainPanel.add(getHeadline());
+		mainPanel.add(intro);
+
 		treeForm.run();
-		// Aufruf des Navigatorbereichs für die Anzeige der abonnierten Hashtags
-		// und User
 		favoriteForm.run();
+
+	}//Ende Run-Methode
+	
+
+	private void showHashtagSubscriptions() {
+		for (User userView : userConList) {
+			
+		}
+
+	}
+
+	private void showPublicConversation() {
+		for (Hashtag hashtagView : hashtagConList){
+			
+		}
 
 	}
 
