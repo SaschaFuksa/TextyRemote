da055b2155b6199411f5852d800d327316e228c0
DanielSeliger <ds138@hdm-stuttgart.de>
2016-01-09 21:45:55 +0100

    Startseite weitgehend fertiggestellt. Das Anzeigen von abonnierten Usern und Hashtags funktioniert mit leichten Problemen bei den abonnierten Usern. Man kann nun auf öffentliche Postings antworten

--- Texty/src/hdm/itprojekt/texty/client/HomeForm.java
+++ Texty/src/hdm/itprojekt/texty/client/HomeForm.java
@@ -1,18 +1,17 @@
 package hdm.itprojekt.texty.client;
 
-import java.util.Vector;
-
 import hdm.itprojekt.texty.shared.TextyAdministrationAsync;
 import hdm.itprojekt.texty.shared.bo.Conversation;
 import hdm.itprojekt.texty.shared.bo.Hashtag;
 import hdm.itprojekt.texty.shared.bo.Message;
 import hdm.itprojekt.texty.shared.bo.User;
 
-import com.google.gwt.cell.client.TextCell;
+import java.util.Vector;
+import java.util.logging.Logger;
+
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FocusPanel;
@@ -21,39 +20,24 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * 
- * Diese Klasse stellt die abonnierten Hashtags und User in zwei verschiedenen
- * ScrollBars im Navigatorbereich dar. 
- *
- */
 public class HomeForm extends TextyForm {
 
-	private static Vector<User> userList = new Vector<User>();
-	private static Vector<Hashtag> hashtagList = new Vector<Hashtag>();
-	private static Vector<Conversation> conListofUser = new Vector<Conversation>();
-	private static Vector<Message> messageListofHashtag = new Vector<Message>();
+	private static final Logger LOG = Logger
+			.getLogger(SingleConversationViewer.class.getSimpleName());
+
+	private Vector<User> userList = new Vector<User>();
+	private Vector<Hashtag> hashtagList = new Vector<Hashtag>();
+	private User currentUser = new User();
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private Label text = new Label("See your:");
 	private Label headerUser = new Label("Subscribed user");
 	private Label headerHashtag = new Label("Subscribed hashtags");
 	private VerticalPanel contentUser = new VerticalPanel();
-	private VerticalPanel userPanel = new VerticalPanel();
-	private ScrollPanel scrollUser = new ScrollPanel(contentUser);
 	private VerticalPanel contentHashtag = new VerticalPanel();
-	private VerticalPanel hashtagPanel = new VerticalPanel();
+	private ScrollPanel scrollUser = new ScrollPanel(contentUser);
 	private ScrollPanel scrollHashtag = new ScrollPanel(contentHashtag);
-	private Vector<String> userVector = new Vector<String>();
-	private Vector<String> hashtagVector = new Vector<String>();
-	private User currentUser = new User();
-	private TextCell textCell = new TextCell();
-	private CellList<String> cellListUser = new CellList<String>(textCell);
-	private CellList<String> cellListHashtag = new CellList<String>(textCell);
-
-	// Detailsbereich für Anzeige der Conversation
-	private VerticalPanel contentConversation = new VerticalPanel();
-	private ScrollPanel scrollConversation = new ScrollPanel(
-			contentConversation);
+	private FlexTable cellListUser = new FlexTable();
+	private FlexTable cellListHashtag = new FlexTable();
 
 	private TextyAdministrationAsync administration = ClientsideSettings
 			.getTextyAdministration();
@@ -62,114 +46,106 @@ public class HomeForm extends TextyForm {
 		super(headline);
 	}
 
-	/**
-	 * Diese Methode holt sich alle abonnierten User und Hashtags und speichert
-	 * sie in den Vectoren userList und hashtagList
-	 */
 	@Override
 	protected void run() {
 
-		// Abonnierte User
-		administration.getAllSubscribedUsers(new AsyncCallback<Vector<User>>() {
-			@Override
-			public void onFailure(Throwable caught) {
-
-			}
-
-			@Override
-			public void onSuccess(Vector<User> result) {
-				userList = result;
-				showSubscribedUser();
-
-				// Abonnierte Hashtags 
-				administration
-						.getAllSubscribedHashtags(new AsyncCallback<Vector<Hashtag>>() {
-							@Override
-							public void onFailure(Throwable caught) {
-
-							}
-
-							@Override
-							public void onSuccess(Vector<Hashtag> result) {
-								hashtagList = result;
-								showSubscribedHashtag();
-								if (result.size() == 0) {
-									headerHashtag.setText("");
-								}
-							}
-
-						});
-			}
-
-		});
-
-		/**
-		 * Diese Methode gibt den aktuell eingeloggten User zurück
-		 */
-		administration.getCurrentUser(new AsyncCallback<User>() {
-			@Override
-			public void onFailure(Throwable caught) {
-
-			}
-
-			@Override
-			public void onSuccess(User result) {
-				currentUser = result;
-			}
-
-		});
+		// Abonnierte User und deren Postings
+		administration.getAllSubscribedUsers(getAllSubscribedUsersExecute());
 
 		this.getElement().setId("fullSize");
-		mainPanel.getElement().setId("fullSize");
-		userPanel.getElement().setId("publicConversation");
+		mainPanel.getElement().setId("conversationWrapper");
 		scrollUser.getElement().setId("conversationScroll");
 		contentUser.getElement().setId("conversationContent");
-		hashtagPanel.getElement().setId("publicConversation");
 		scrollHashtag.getElement().setId("conversationScroll");
 		contentHashtag.getElement().setId("conversationContent");
 
 		contentUser.add(cellListUser);
 		contentHashtag.add(cellListHashtag);
-		userPanel.add(scrollUser);
-		hashtagPanel.add(scrollHashtag);
 
 		mainPanel.add(getHeadline());
 		mainPanel.add(text);
 		mainPanel.add(headerUser);
-		mainPanel.add(userPanel);
+		mainPanel.add(scrollUser);
 		mainPanel.add(headerHashtag);
-		mainPanel.add(hashtagPanel);
+		mainPanel.add(scrollHashtag);
 
 		this.add(mainPanel);
 
 		Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
 			public void execute() {
-				scrollUser.setHeight(mainPanel.getOffsetHeight() * 5 / 16
-						+ "px");
-				scrollHashtag.setHeight(mainPanel.getOffsetHeight() * 5 / 16
+				scrollUser.setHeight(mainPanel.getOffsetHeight() * 0.45 + "px");
+				scrollHashtag.setHeight(mainPanel.getOffsetHeight() * 0.45
 						+ "px");
 			}
 		});
 
 	}// Ende Run-Methode
 
-	/**
-	 * Diese Methode wählt in einem Schleifendurchlauf alle beinhalteten User an
-	 * und bezieht dessen Vor -und Nachname, nachdem jeder User an die Methode createUserTable übergeben wurden. 
-	 * Des weiteren kann man auch seine persönliche Chronik aller öffentlichen Messages einsehen.
-	 */
-	private void showSubscribedUser() {
+	private AsyncCallback<Vector<User>> getAllSubscribedUsersExecute() {
+		AsyncCallback<Vector<User>> asyncCallback = new AsyncCallback<Vector<User>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				LOG.severe("Error: " + caught.getMessage());
+			}
 
-		createUserTable(currentUser, true);
+			@Override
+			public void onSuccess(Vector<User> result) {
+				LOG.info("Success :" + result.getClass().getSimpleName());
+				userList = result;
+				administration.getCurrentUser(getCurrentUserExecuter());
+			}
 
-		for (User user : userList) {
-			createUserTable(user, false);
-		}
-
+		};
+		return asyncCallback;
 	}
 
-	private void createUserTable(User user, boolean isCurrentUser) {
-		final User userView = user;
+	private AsyncCallback<User> getCurrentUserExecuter() {
+		AsyncCallback<User> asyncCallback = new AsyncCallback<User>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				LOG.severe("Error: " + caught.getMessage());
+
+			}
+
+			@Override
+			public void onSuccess(User result) {
+				LOG.info("Success :" + result.getClass().getSimpleName());
+				currentUser = result;
+				createUserTable(currentUser, true);
+				for (User user : userList) {
+					createUserTable(user, false);
+				}
+				// Abonnierte Hashtags und deren Postings
+				administration
+						.getAllSubscribedHashtags(getAllSubscribedHashtagsExecute());
+			}
+		};
+		return asyncCallback;
+	}
+
+	private AsyncCallback<Vector<Hashtag>> getAllSubscribedHashtagsExecute() {
+		AsyncCallback<Vector<Hashtag>> asyncCallback = new AsyncCallback<Vector<Hashtag>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				LOG.severe("Error: " + caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(Vector<Hashtag> result) {
+				LOG.info("Success :" + result.getClass().getSimpleName());
+				hashtagList = result;
+				showSubscribedHashtag();
+				if (result.size() == 0) {
+					headerHashtag.setText("");
+				}
+			}
+
+		};
+		return asyncCallback;
+	}
+
+	// Navigatorbereich User
+	private void createUserTable(final User user, boolean isCurrentUser) {
 		FlexTable chatFlexTable = new FlexTable();
 		FocusPanel wrapper = new FocusPanel();
 		String userName = new String();
@@ -177,7 +153,7 @@ public class HomeForm extends TextyForm {
 		if (isCurrentUser) {
 			userName = "Your own public postings";
 		} else {
-			userName = userView.getFirstName() + " " + userView.getLastName();
+			userName = user.getFirstName() + " " + user.getLastName();
 		}
 
 		Label userLabel = new Label(userName);
@@ -187,7 +163,8 @@ public class HomeForm extends TextyForm {
 			@Override
 			public void onClick(ClickEvent event) {
 
-				showMessageSelectedUser(userView);
+				administration.getAllPublicConversationsFromUser(user,
+						getAllPublicConversationsFromUserExecute(user));
 
 			}
 		});
@@ -198,19 +175,39 @@ public class HomeForm extends TextyForm {
 		contentUser.add(wrapper);
 	}
 
-	/**
-	 * Diese Methode wählt in einem Schleifendurchlauf alle beinhalteten
-	 * Hashtags an und bezieht dessen Keywords, die auf einem Label platziert
-	 * werden
-	 */
+	private AsyncCallback<Vector<Conversation>> getAllPublicConversationsFromUserExecute(
+			final User user) {
+		AsyncCallback<Vector<Conversation>> asyncCallback = new AsyncCallback<Vector<Conversation>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				LOG.severe("Error: " + caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(Vector<Conversation> result) {
+				LOG.info("Success :" + result.getClass().getSimpleName());
+				TextyForm publicConversationViewer = new PublicConversationViewer(
+						"Public Postings from " + user.getFirstName(), result);
+
+				RootPanel.get("Details").clear();
+				RootPanel.get("Info").clear();
+				RootPanel.get("Details").add(publicConversationViewer);
+
+			}
+
+		};
+		return asyncCallback;
+	}
+
+	// Navigatorbereich Hashtags
 	private void showSubscribedHashtag() {
 
-		for (Hashtag hashtag : hashtagList) {
-			final Hashtag hashtagView = hashtag;
+		for (final Hashtag hashtag : hashtagList) {
 			FlexTable chatFlexTable = new FlexTable();
 			FocusPanel wrapper = new FocusPanel();
 
-			String keyword = hashtagView.getKeyword();
+			String keyword = "#" + hashtag.getKeyword();
 			Label hashtagLabel = new Label(keyword);
 			hashtagLabel.getElement().setId("conversationHead");
 			wrapper.addClickHandler(new ClickHandler() {
@@ -218,7 +215,8 @@ public class HomeForm extends TextyForm {
 				@Override
 				public void onClick(ClickEvent event) {
 
-					showMessageSelectedHashtag(hashtagView);
+					administration.getAllPublicMessagesFromHashtag(hashtag,
+							getAllPublicMessagesFromHashtagExecute(hashtag));
 				}
 			});
 
@@ -226,68 +224,33 @@ public class HomeForm extends TextyForm {
 			chatFlexTable.getElement().setId("conversation");
 			wrapper.add(chatFlexTable);
 			contentHashtag.add(wrapper);
+
 		}
 
 	}
 
-	/**
-	 * Hier entsteht die Anzeige der User im Detailsbereich. Der angewählte User
-	 * wird, nachdem mir von der Applikations-Ebene dessen Conversations
-	 * geliefert wurden, an die Klasse PublicConversationViewer übergeben
-	 * 
-	 */
-	public void showMessageSelectedUser(final User user) {
-
-		administration.getAllPublicConversationsFromUser(user,
-				new AsyncCallback<Vector<Conversation>>() {
-
-					@Override
-					public void onFailure(Throwable caught) {
-
-					}
-
-					@Override
-					public void onSuccess(Vector<Conversation> result) {
-						TextyForm publicConversationViewer = new PublicConversationViewer(
-								"Public Postings from " + user.getFirstName(),
-								result);
-
-						RootPanel.get("Details").clear();
-						RootPanel.get("Details").add(publicConversationViewer);
-
-					}
-
-				});
-
-	}
-
-	/**
-	 * Hier entsteht die Anzeige der Hashtags im Detailsbereich. Der angewählte
-	 * Hashtag wird an die Klasse PublicHashtagViewer übergeben.
-	 * 
-	 */
 	// Hier entsteht die Detailsanzeige der Hashtags
-	public void showMessageSelectedHashtag(final Hashtag hashtag) {
+	public AsyncCallback<Vector<Message>> getAllPublicMessagesFromHashtagExecute(final Hashtag hashtag) {
 
-		administration.getAllPublicMessagesFromHashtag(hashtag,
-				new AsyncCallback<Vector<Message>>() {
+		AsyncCallback<Vector<Message>> asyncCallback = new AsyncCallback<Vector<Message>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				LOG.severe("Error: " + caught.getMessage());
+			}
 
-					@Override
-					public void onFailure(Throwable caught) {
+			@Override
+			public void onSuccess(Vector<Message> result) {
+				LOG.info("Success :" + result.getClass().getSimpleName());
+				TextyForm publicHashtagViewer = new PublicHashtagViewer(
+						"Public Posting with hashtag #" + hashtag.getKeyword(), result);
 
-					}
+				RootPanel.get("Details").clear();
+				RootPanel.get("Info").clear();
+				RootPanel.get("Details").add(publicHashtagViewer);
+			}
 
-					@Override
-					public void onSuccess(Vector<Message> result) {
-						TextyForm publicHashtagViewer = new PublicHashtagViewer(
-								"Public Hashtags of " + hashtag.getKeyword(),
-								result);
-
-						RootPanel.get("Details").clear();
-						RootPanel.get("Details").add(publicHashtagViewer);
-					}
-
-				});
+		};
+		return asyncCallback;
 
 	}
 
--- Texty/src/hdm/itprojekt/texty/client/PublicHashtagViewer.java
+++ Texty/src/hdm/itprojekt/texty/client/PublicHashtagViewer.java
@@ -2,28 +2,33 @@ package hdm.itprojekt.texty.client;
 
 import hdm.itprojekt.texty.shared.bo.Hashtag;
 import hdm.itprojekt.texty.shared.bo.Message;
-import java.util.Vector;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import java.util.Date;
+import java.util.Vector;
 
 import com.google.gwt.i18n.shared.DateTimeFormat;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * 
- * Diese Klasse wird in der Klasse HomeForm aufgerufen und dient der Darstellung
- * aller öffentlichen Nachrichten des im Editor ausgewählten Hashtags. Weiterhin
+ * Diese Klasse wird in der Klasse HomeForm verwendet und dient der Darstellung
+ * aller öffentlichen Nachrichten eines Users, die den abonnierten Hashtag beinhalten. Weiterhin
  * werden alle weiteren Hashtags, die in der Message enthalten sind, angezeigt
  *
  */
 public class PublicHashtagViewer extends TextyForm {
+	
+	private final static long ONE_MINUTE = 60;
+	private final static long ONE_HOURS = 60 * ONE_MINUTE;
+	private final static long ONE_DAYS = 24 * ONE_HOURS;
+	private final static long ONE_MONTH = 30 * ONE_DAYS;
 
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private Vector<Message> messageListofHashtag = new Vector<Message>();
+	private Label text = new Label(
+			"Here you can see all public messages with your favourite hashtag!");
 
 	public PublicHashtagViewer(String headline,
 			Vector<Message> messageListofHashtag) {
@@ -34,10 +39,16 @@ public class PublicHashtagViewer extends TextyForm {
 	@Override
 	protected void run() {
 
+		if (messageListofHashtag.size() == 0) {
+			text.setText("Oops! This hashtag doesn't exist in any public conversations!");
+		}
+		
 		this.getElement().setId("fullSize");
 		mainPanel.getElement().setId("fullWidth");
+		text.getElement().setId("blackFont");
 
 		mainPanel.add(getHeadline());
+		mainPanel.add(text);
 
 		addConversation();
 
@@ -46,57 +57,69 @@ public class PublicHashtagViewer extends TextyForm {
 	}
 
 	private void addConversation() {
-		for (Message message : messageListofHashtag) {
-			final Message messageView = message;
+		for (final Message message : messageListofHashtag) {
 
-			messageView.getListOfHashtag();
-
-			final VerticalPanel hashtagPanel = new VerticalPanel();
 			FlexTable chatFlexTable = new FlexTable();
-			FocusPanel wrapper = new FocusPanel();
-			String date = DateTimeFormat.getFormat("yyyy-MM-dd").format(
-					messageView.getDateOfCreation());
-			Label dateLabel = new Label(date);
 
-			Vector<Hashtag> hashtagVector = messageView.getListOfHashtag();
+			String dateString = DateTimeFormat.getFormat("HH:mm").format(
+					message.getDateOfCreation());
 
-			String text = messageView.getText();
-			final Label textLabel = new Label(text);
+			Date afterDate = new Date();
+			Date baseDate = message.getDateOfCreation();
 
-			hashtagPanel.getElement().setId("conversation");
+			long afterTime = afterDate.getTime() / 1000;
+			long baseTime = baseDate.getTime() / 1000;
 
-			wrapper.addClickHandler(new ClickHandler() {
-				@Override
-				public void onClick(ClickEvent event) {
-					// for (int i = 1; i < messageView; i++) {
-					/*
-					 * VerticalPanel singleHashtagPanel = new VerticalPanel();
-					 * Label messageText = new Label(messageView .getText());
-					 * singleHashtagPanel.add(messageText);
-					 * hashtagPanel.add(singleHashtagPanel);
-					 */
+			long duration = afterTime - baseTime;
 
-					// RootPanel.get("Info").clear();
-					// RootPanel.get("Details").add(textLabel);
+			if (duration < ONE_DAYS) {
+				String baseDay = DateTimeFormat.getFormat("dd").format(baseDate);
+				String afterDay = DateTimeFormat.getFormat("dd").format(afterDate);
+
+				if (!baseDay.equals(afterDay)) {
+					dateString = "yesterday at " + dateString;
 				}
-			});
 
-			// chatFlexTable.setWidget(0, 0, authorLabel);
-			chatFlexTable.setWidget(0, 0, dateLabel);
-			// chatFlexTable.setWidget(1, 0, receiverLabel);
-			chatFlexTable.setWidget(1, 0, textLabel);
-
-			wrapper.add(chatFlexTable);
-			hashtagPanel.add(wrapper);
-
-			for (int i = 0; i < hashtagVector.size(); i++) {
-				Label hashtagText = new Label("#"
-						+ messageView.getListOfHashtag().get(i).getKeyword()
-						+ " ");
-				hashtagPanel.add(hashtagText);
+			} else if (duration > ONE_DAYS && duration < ONE_MONTH) {
+				dateString = DateTimeFormat.getFormat("dd.MM").format(baseDate)
+						+ " at "
+						+ DateTimeFormat.getFormat("HH").format(baseDate) + "h";
 			}
 
-			mainPanel.add(hashtagPanel);
+			else if (duration > ONE_MONTH) {
+				dateString = DateTimeFormat.getFormat("MM.yyyy").format(
+						baseDate)
+						+ " at "
+						+ DateTimeFormat.getFormat("dd").format(baseDate)
+						+ ", "
+						+ DateTimeFormat.getFormat("HH").format(baseDate) + "h";
+			}
+			
+			String keywordList = new String();
+
+			for (Hashtag hashtag : message.getListOfHashtag()) {
+				keywordList = keywordList + "#" + hashtag.getKeyword() + " ";
+			}
+			
+			Label authorLabel = new Label(message.getAuthor().getFirstName());
+			Label dateLabel = new Label(dateString);
+			Label hashtagLabel = new Label(keywordList);
+
+			String text = message.getText();
+			final Label textLabel = new Label(text);
+
+			dateLabel.getElement().setId("floatRight");
+			chatFlexTable.getElement().setId("conversation");
+
+			chatFlexTable.getFlexCellFormatter().setColSpan(1, 0, 2);
+			chatFlexTable.getFlexCellFormatter().setColSpan(2, 0, 2);
+			
+			chatFlexTable.setWidget(0, 0, authorLabel);
+			chatFlexTable.setWidget(0, 1, dateLabel);
+			chatFlexTable.setWidget(1, 0, textLabel);
+			chatFlexTable.setWidget(2, 0, hashtagLabel);
+
+			mainPanel.add(chatFlexTable);
 
 		} // Ende for-Schleife
 	}
